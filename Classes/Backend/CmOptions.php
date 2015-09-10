5613611a794374346d6d196f6fe88327ca15f692
René Fritz <rf@colorcube.de>
2015-09-10 15:46:07 +0200

    Fix context menu which broke clipboard in page module. Remove old code from content menu code which won't work anymore (GridElementsDD) Not shure what it was about.

--- Classes/Backend/CmOptions.php
+++ Classes/Backend/CmOptions.php
@@ -24,13 +24,6 @@ class CmOptions {
 	 */
 	public function main(ClickMenu &$backRef, array $menuItems, $table, $uid) {
 
-		// add copied item handler to "(un)copy" link in clickmenu
-		if (strpos($menuItems['copy'][0], 't3-icon-edit-copy-release') === FALSE) {
-			preg_match('@&uid=(?P<digit>\d+)&@', $menuItems['copy'][3], $arrMatches);
-			$strUidInLink = $arrMatches[1];
-			$menuItems['copy'][3] = str_replace('return false;', ' GridElementsDD.listenForCopyItem(' . $strUidInLink . '); return false;', $menuItems['copy'][3]);
-		}
-
 		// add "paste reference after"
 		$parkItem = $menuItems['pasteafter'];
 		if ($parkItem) {
